500a96d35c21d0553ba2166b16bbd33a068575fe
Perry Fustero <pfustero@goldschmitt.com>
2021-02-12 11:20:04 -0500

    unfortunate truth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { Button, Modal, Dropdown } from 'react-bootstrap'
+import { Button, Modal, Dropdown, DropdownButton } from 'react-bootstrap'
+import DropdownItem from 'react-bootstrap/esm/DropdownItem'
 
 const App = () => {
 	const [showModal, setShowModal] = useState(false)
@@ -11,6 +12,16 @@ const App = () => {
 		<div>
 			<h1>Dropdown Test</h1>
 			<Button onClick={handleShow}>Open Modal</Button>
+
+			<DropdownButton
+				id="dropdown-test"
+				title="Dropdown Button"
+				variant="secondary"
+			>
+				<DropdownItem>1</DropdownItem>
+				<DropdownItem>2</DropdownItem>
+				<DropdownItem>3</DropdownItem>
+			</DropdownButton>
 
 			<Modal
 				animation={false}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+// ** @jsxRuntime classic */ -- For IE11
+
+import 'react-app-polyfill/ie11' // For IE 11 support
+import 'react-app-polyfill/stable'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
+import './index.css'
 
 ReactDOM.render(
 	<React.StrictMode>
