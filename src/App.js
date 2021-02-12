eefaf3943774ea26be8a3d8827cfd40adf1bf3a9
Perry Fustero <pfustero@goldschmitt.com>
2021-02-12 10:13:51 -0500

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Button, Modal, Dropdown } from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const [showModal, setShowModal] = useState(false)
+
+	const handleClose = () => setShowModal(false)
+	const handleShow = () => setShowModal(true)
+
+	return (
+		<div>
+			<h1>Dropdown Test</h1>
+			<Button onClick={handleShow}>Open Modal</Button>
+
+			<Modal
+				animation={false}
+				show={showModal}
+				onHide={handleClose}
+				backdrop="static"
+			>
+				<Modal.Header>Hiya!</Modal.Header>
+				<Modal.Body>
+					<Button onClick={handleClose}>Close</Button>
+				</Modal.Body>
+			</Modal>
+		</div>
+	)
 }
 
-export default App;
+export default App
